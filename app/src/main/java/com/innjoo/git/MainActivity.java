ca9e7c60376d097e9281c1be9e9282b4be4b6ba4
shitangzhilang <834461503@qq.com>
2016-03-29 14:44:19 +0800

    2

--- app/src/main/java/com/innjoo/git/MainActivity.java
+++ app/src/main/java/com/innjoo/git/MainActivity.java
@@ -1,7 +1,8 @@
 package com.innjoo.git;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv_test = (TextView) findViewById(R.id.tv_test);
+        tv_test.setText("wolegeca");
     }
 }
